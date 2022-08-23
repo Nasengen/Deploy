987055977758909a7002459910a2ed47f4c9f24f
Anne Marie <verouskgains@gmail.com>
2022-08-23 02:40:18 +0000

    client and QA appoved

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ deploy java applictaion
 deploy python apps
 deploy in the dev env
 deployement5 master
-deployemnt4
-deployement4
-deployement4
+deployemnt4 Dev
+deployement4 stage
+deployement4 Deployment
 
